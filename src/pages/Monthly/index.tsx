878b8c9e9a73492a0f2d1c8bec72b3b3e45d4a1c
Victor Radael <victorradael@hotmail.com>
2020-07-04 21:28:41 -0300

    :rocket: Only One Challenge

--- src/pages/Monthly/index.tsx
+++ src/pages/Monthly/index.tsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { GoCheck } from "react-icons/go";
 
 import api from "../../services/api";
 import {
@@ -9,6 +11,9 @@ import {
   Plans,
   Title,
   Button,
+  Switch,
+  PlanItem,
+  ButtonPeriod,
 } from "./styles";
 import dialog from "../../assets/dialog.svg";
 
@@ -23,21 +28,93 @@ interface IPlans {
   features: [];
 }
 
-interface IAttendant {
+interface IPlanSelected {
+  id: number;
+  name: string;
+  price: number;
+}
+
+interface IAttendantCoast {
   cost: number;
 }
 
+interface IPeriod {
+  name: string;
+  value: string;
+}
+
 const Monthly: React.FC = () => {
-  const [plans, setPlans] = useState<IPlans[]>();
-  const [attendantCoast, setAttendantCoast] = useState<IAttendant>();
+  const [plans, setPlans] = useState<IPlans[]>([]);
+  const [attendantCost, setAttendantCost] = useState<IAttendantCoast>();
+  const [selected, setSelected] = useState<IPlanSelected>({} as IPlanSelected);
+  const [periodSelect, setPeriodSelected] = useState<IPeriod>({} as IPeriod);
+  const [numberOfAttendants, setNumberOfAttendants] = useState<number>(0);
+  const [total, setTotal] = useState<number>();
+
+  const loadData = useCallback(async () => {
+    const attendant = await api.get("attendant");
+    setAttendantCost(attendant.data);
+
+    const responsePlans = await api.get("plans");
+    const dataPlans: IPlans[] = responsePlans.data;
+    setPlans(dataPlans);
+
+    setSelected({
+      id: dataPlans[1].id,
+      name: dataPlans[1].name,
+      price: dataPlans[1].prices.monthly,
+    });
+    setTotal(dataPlans[1].prices.monthly);
+    setPeriodSelected({ name: "Mensal", value: "monthly" });
+  }, []);
 
   useEffect(() => {
-    api.get("plans").then((response) => setPlans(response.data));
-    api.get("attendant").then((response) => setAttendantCoast(response.data));
-  }, []);
+    loadData();
+  }, [loadData]);
+
+  const calcTotalAddAttendant = useCallback(() => {
+    if (attendantCost) {
+      const result =
+        (numberOfAttendants + 1) * attendantCost.cost + selected.price;
+
+      setTotal(result);
+    }
+  }, [attendantCost, numberOfAttendants, selected.price]);
+
+  const calcTotalRemoveAttendant = useCallback(() => {
+    if (attendantCost) {
+      const result =
+        (numberOfAttendants - 1) * attendantCost.cost + selected.price;
+
+      setTotal(result);
+    }
+  }, [attendantCost, numberOfAttendants, selected.price]);
+
+  const periods = [
+    { name: "Mensal", value: "monthly" },
+    {
+      name: "Anual",
+      value: "yearly",
+    },
+  ];
 
   return (
     <Container>
+      <Switch>
+        <div>
+          {periods.map((type) => (
+            <ButtonPeriod
+              key={type.value}
+              onClick={() =>
+                setPeriodSelected({ name: type.name, value: type.value })
+              }
+              periodSelected={periodSelect.value === type.value ? true : false}
+            >
+              {type.name}
+            </ButtonPeriod>
+          ))}
+        </div>
+      </Switch>
       <Plans>
         {plans &&
           plans.map((plan) => (
@@ -50,9 +127,25 @@ const Monthly: React.FC = () => {
                 </Title>
                 <h6>{plan.description}</h6>
                 <h3>R${plan.prices.monthly}/mês</h3>
-                <Button>Selecionar</Button>
+                <Button
+                  onClick={() =>
+                    setSelected({
+                      id: plan.id,
+                      name: plan.name,
+                      price: plan.prices.monthly,
+                    })
+                  }
+                  selected={selected.id === plan.id ? true : false}
+                >
+                  Selecionar
+                </Button>
                 {plan.features.map((item) => (
-                  <p key={item}>{item}</p>
+                  <PlanItem key={item}>
+                    <div>
+                      <GoCheck size={10} />
+                    </div>
+                    <p>{item}</p>
+                  </PlanItem>
                 ))}
               </div>
             </PlanBox>
@@ -60,22 +153,40 @@ const Monthly: React.FC = () => {
       </Plans>
 
       <SubFooter>
-        <div>
+        <section>
           <h3>Atendentes</h3>
-          <p>+R${attendantCoast?.cost}/mês por atendente</p>
-        </div>
+          <p>+R${attendantCost?.cost}/mês por atendente</p>
+        </section>
         <div>
-          <p> menor </p>
-          <h3>1</h3>
-          <p> maior </p>
+          <FiChevronLeft
+            size={20}
+            cursor="pointer"
+            onClick={() => {
+              if (numberOfAttendants > 0) {
+                setNumberOfAttendants(numberOfAttendants - 1);
+                calcTotalRemoveAttendant();
+              }
+            }}
+          />
+          <h3>{numberOfAttendants}</h3>
+          <FiChevronRight
+            size={20}
+            cursor="pointer"
+            onClick={() => {
+              setNumberOfAttendants(numberOfAttendants + 1);
+              calcTotalAddAttendant();
+            }}
+          />
         </div>
       </SubFooter>
       <Footer>
+        <section>
+          <h3>Total: R${total}/mês</h3>
+          <p>Plano Selecionado: {selected.name} - Mensal</p>
+        </section>
         <div>
-          <h3>Total:R$380/mês</h3>
-          <p>Plano Selecionado: Plano 2.0 - Mensal</p>
+          <button onClick={() => alert("Teste")}>Contratar</button>
         </div>
-        <button>Contratar</button>
       </Footer>
     </Container>
   );
--- src/pages/Monthly/styles.ts
+++ src/pages/Monthly/styles.ts
@@ -1,11 +1,54 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+interface IProps {
+  selected?: boolean;
+  periodSelected?: boolean;
+}
 
 export const Container = styled.div`
   margin: 56px 0;
-  padding: 8px;
+
   box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.5);
   border-radius: 16px;
+  font-family: "Lato", sans-serif;
 `;
+
+export const Switch = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 24px;
+
+  div {
+    background: #f4f7fc;
+    box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
+    border-radius: 100px;
+  }
+`;
+
+export const ButtonPeriod = styled.button<IProps>`
+  border-radius: 100px;
+  background: #f4f7fc;
+  cursor: pointer;
+  padding: 8px 24px;
+  color: #333;
+  font-weight: bold;
+  border: none;
+
+  &:focus {
+    box-shadow: 0 0 0 0;
+    outline: 0;
+  }
+
+  ${(props) =>
+    props.periodSelected === true &&
+    css`
+      background: #00a6ce;
+      color: #fff;
+    `}
+`;
+
 export const Plans = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
@@ -16,18 +59,21 @@ export const PlanBox = styled.div`
   flex-direction: column;
   align-items: center;
   box-sizing: border-box;
-  padding: 16px;
-  font-family: "Lato", sans-serif;
+  padding: 0 40px;
 
-  p {
-    margin-top: 4px;
-  }
+  border-left: 1px solid #ddd;
 
   h6 {
-    margin: 4px 0 0 0;
+    margin: 8px 0 0 0;
     font-size: 14px;
     font-weight: normal;
     color: #999;
+  }
+
+  h3 {
+    font-size: 32px;
+    color: #00a6ce;
+    margin: 16px 0 0 0;
   }
 `;
 
@@ -45,22 +91,127 @@ export const Title = styled.div`
   }
 `;
 
-export const Button = styled.button`
+export const Button = styled.button<IProps>`
   width: 300px;
   border-radius: 100px;
   border: none;
-  background: #43b998;
-  color: #fff;
+  background: #f4f7fc;
+  color: #333;
   font-weight: bold;
   font-family: "Lato", sans-serif;
   font-size: 18px;
   padding: 8px 0;
   margin-bottom: 32px;
+  cursor: pointer;
+
+  &:focus {
+    box-shadow: 0 0 0 0;
+
+    outline: 0;
+  }
+
+  ${(props) =>
+    props.selected === true &&
+    css`
+      background: #43b998;
+      color: #fff;
+    `}
+`;
+
+export const PlanItem = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 8px;
+
+  div {
+    color: #fff;
+    background: #43b998;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 3px;
+    margin: 0px 8px;
+  }
+  p {
+    margin: 0;
+    color: rgba(0, 0, 0, 0.8);
+  }
 `;
 
 export const SubFooter = styled.div`
   box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  margin: 0 0 16px 40px;
+
+  section {
+    margin-right: 16px;
+    h3 {
+      margin: 0;
+      font-weight: normal;
+      font-size: 30px;
+    }
+    p {
+      margin: 0;
+      color: #00a6ce;
+      font-size: 14px;
+    }
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    color: #00a6ce;
+
+    h3 {
+      font-weight: normal;
+      font-size: 32px;
+      margin: 0 4px;
+    }
+  }
 `;
 export const Footer = styled.div`
   box-sizing: border-box;
+  background: #00a6ce;
+  border-radius: 0 0 16px 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  section {
+    padding: 24px;
+    color: #fff;
+    h3 {
+      margin: 0;
+      font-size: 30px;
+    }
+
+    p {
+      margin: 0;
+      color: #f5f5f5;
+      font-size: 16px;
+    }
+  }
+
+  div {
+    padding-right: 24px;
+    button {
+      width: 200px;
+      border-radius: 100px;
+      border: none;
+      background: #43b998;
+      color: #fff;
+      font-weight: bold;
+      font-size: 18px;
+      padding: 8px 0;
+      cursor: pointer;
+
+      &:focus {
+        box-shadow: 0 0 0 0;
+
+        outline: 0;
+      }
+    }
+  }
 `;
